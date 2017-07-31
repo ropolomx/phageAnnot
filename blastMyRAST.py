5374ae85720da59d217c95084975dca87f36fc1f
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-07-31 13:33:11 -0600

    Parse Genbank CDS translations into new SeqRecords, and onvert to fasta

--- blastMyRAST.py
+++ blastMyRAST.py
@@ -1,11 +1,39 @@
-from Bio import SeqIO
-from Bio import Bio.Blast.Applications
+#! /usr/bin/env python3
 
-recs = [r for r in SeqIO.parse(myRast, 'genbank')
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+
+# Parse Genbank file
+
+myRASTGB = [r for r in SeqIO.parse(myRast, 'genbank')
+
+# Converting CDS regions from Gebank file to FASTA for blastp/psiblast search
+
+headers = []
+
+translations = []
 
 for r in recs:
     for feature in r.features:
         if feature.type == "CDS":
-            print(feature.qualifiers['translation'])
+            headers.append('_'.join([r.name, feature.qualifiers['locus_tag'][0], feature.qualifiers['product'][0]]))
+            translations.append(feature.qualifiers['translation'][0])
 
- 
+# Zip headers and translated sequences into a zip object with tuples
+# Returns an iterator of tuples
+
+fastaTuple = zip(headers, translations) 
+
+# List comprehension to generate new sequence records from the information
+# generated in the tuple above
+
+# TODO: fix description
+
+newRecs = [SeqRecord(Seq(f[1], IUPAC.protein),id=f[0], name=f[0]) for f in fastaTuple]
+
+# Write new records as FASTA
+
+SeqIO.write(newRecs, 'newRecs.fasta', 'fasta')
