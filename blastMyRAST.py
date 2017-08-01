d52efe871f7f7dfbb8291ae711baa220bcef8910
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2017-08-01 10:38:10 -0600

    Add descriptions to FASTA header

--- blastMyRAST.py
+++ blastMyRAST.py
@@ -14,25 +14,29 @@ myRASTGB = [r for r in SeqIO.parse(myRast, 'genbank')
 
 headers = []
 
+descriptions = []
+
 translations = []
 
 for r in recs:
     for feature in r.features:
         if feature.type == "CDS":
-            headers.append('_'.join([r.name, feature.qualifiers['locus_tag'][0], feature.qualifiers['product'][0]]))
+            headers.append('_'.join([r.name, feature.qualifiers['locus_tag'][0]) 
+            descriptions.append(feature.qualifiers['product'][0]])) 
             translations.append(feature.qualifiers['translation'][0])
 
-# Zip headers and translated sequences into a zip object with tuples
+
+# Zip headers, descriptions, and translated sequences into a zip object with tuples
 # Returns an iterator of tuples
 
-fastaTuple = zip(headers, translations) 
+fastaTuple = zip(headers, descriptions, translations) 
 
 # List comprehension to generate new sequence records from the information
 # generated in the tuple above
 
 # TODO: fix description
 
-newRecs = [SeqRecord(Seq(f[1], IUPAC.protein),id=f[0], name=f[0]) for f in fastaTuple]
+newRecs = [SeqRecord(Seq(f[2], IUPAC.protein), id=f[0], name=f[0], description=f[1]) for f in fastaTuple]
 
 # Write new records as FASTA
 
